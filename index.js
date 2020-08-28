67d81d745f347ff9fd0ec675ab0c8c4214a53219
Artur Lupolov <artur_1997_1997@mail.ru>
2020-08-28 16:00:45 +0300

    commit new save index.js

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Hello from './src/Hello';
+import check from './src/Hello';
+//import check from './src/Hello';
 
 ReactDOM.render(
-    <Hello />,
+    <check />,
     document.getElementById('root')
 )
